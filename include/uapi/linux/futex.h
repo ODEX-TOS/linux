c80a71438200564ccfd82215a5fe35dd2dc6bb3c
André Almeida <andrealmeid@collabora.com>
2021-04-27 20:12:38 -0300

    futex2: Implement vectorized wait
    Add support to wait on multiple futexes. This is the interface
    implemented by this syscall:
    
    futex_waitv(struct futex_waitv *waiters, unsigned int nr_futexes,
    	    unsigned int flags, struct timespec *timo)
    
    struct futex_waitv {
    	__u64 val;
    	void *uaddr;
    	unsigned int flags;
    };
    
    Given an array of struct futex_waitv, wait on each uaddr. The thread
    wakes if a futex_wake() is performed at any uaddr. The syscall returns
    immediately if any waiter has *uaddr != val. *timo is an optional
    timeout value for the operation. The flags argument of the syscall
    should be used solely for specifying the timeout as realtime, if needed.
    Flags for shared futexes, sizes, etc. should be used on the individual
    flags of each waiter.
    
    Returns the array index of one of the awakened futexes. There’s no given
    information of how many were awakened, or any particular attribute of it
    (if it’s the first awakened, if it is of the smaller index...).
    
    Signed-off-by: André Almeida <andrealmeid@collabora.com>

--- include/uapi/linux/futex.h
+++ include/uapi/linux/futex.h
@@ -48,6 +48,20 @@
 
 #define FUTEX_SHARED_FLAG 8
 
+#define FUTEX_WAITV_MAX 128
+
+/**
+ * struct futex_waitv - A waiter for vectorized wait
+ * @val:   Expected value at uaddr
+ * @uaddr: User address to wait on
+ * @flags: Flags for this waiter
+ */
+struct futex_waitv {
+	__u64 val;
+	void __user *uaddr;
+	unsigned int flags;
+};
+
 /*
  * Support for robust futexes: the kernel cleans up held futexes at
  * thread exit time.
