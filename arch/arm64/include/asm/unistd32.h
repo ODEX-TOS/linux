538cede461c084ca65808ece45648ce171a3f39b
Steven Barrett <steven@liquorix.net>
2021-05-04 13:32:41 -0500

    Revert "Merge remote-tracking branch 'github/5.12/futex2' into 5.12/master"
    Prepare for updated futex2-proton code.
    
    This reverts commit 06e43d166805a8dde231495ab4f9622962824163, reversing
    changes made to 1196de115c36a691f88709ab0a02abfbf6be2fdc.

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -893,14 +893,6 @@ __SYSCALL(__NR_process_madvise, sys_process_madvise)
 __SYSCALL(__NR_epoll_pwait2, compat_sys_epoll_pwait2)
 #define __NR_mount_setattr 442
 __SYSCALL(__NR_mount_setattr, sys_mount_setattr)
-#define __NR_futex_wait 443
-__SYSCALL(__NR_futex_wait, sys_futex_wait)
-#define __NR_futex_wake 444
-__SYSCALL(__NR_futex_wake, sys_futex_wake)
-#define __NR_futex_waitv 445
-__SYSCALL(__NR_futex_waitv, compat_sys_futex_waitv)
-#define __NR_futex_waitv 446
-__SYSCALL(__NR_futex_requeue, compat_sys_futex_requeue)
 
 /*
  * Please add new compat syscalls above this comment and update
